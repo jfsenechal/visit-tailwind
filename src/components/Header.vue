bf6e3d78c9993a56317e0ff0f0a28b63297b70af
jfsenechal <jfsenechal@gmail.com>
2021-12-15 15:21:59 +0100

    nav, icones

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,9 +4,9 @@ import IconesSocial from "./IconesSocial.vue";
 </script>
 
 <template>
-  <header class="flex justify-between items-center p-3">
+  <header class="flex justify-between items-center p-3 bg-">
     <a href="#" class="flex flex-grow flex-shrink-0">
-      <img class="w-70 h-40 inline" alt="logo"
+      <img class="w-24 inline" alt="logo"
            src="https://visitmarche.be/wp-content/themes/visitmarche/assets/tartine/rsc/img/img_logo.png"/>
     </a>
     <nav class="ml-au45to">
