0c319f67e77aecba9d7cc51a9a74aea0d812ebef
jfsenechal <jfsenechal@gmail.com>
2021-12-18 21:53:50 +0100

    banner home

---
+++ src/components/BannerParty.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="bg-party flex justify-start items-center relative text-white px-5 overflow-visible">
+    <img class="object-[initial] relative -top-8 h-48 w-80"
+         src="https://visitmarche.be/wp-content/themes/visitmarche/assets/tartine/rsc/img/img_cat01.png" alt="party"/>
+    <h2 class="text-2xl">Connaissez-vous nos différentes festivités ?</h2>
+
+    <a href="#"
+       class="text-center flex-shrink-0 w-44 ml-auto rounded-full text-lg border-2 border-white hover:bg-white hover:text-grey-dark">Découvrez-les
+      !</a>
+  </div>
+</template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -18,7 +18,11 @@ module.exports = {
                 party: '#EFD7CDFF',
                 home: '#E8DACBFF',
                 caractere: '#636061',
-                borderjf: '#dee2e6'
+                borderjf: '#dee2e6',
+                grey: {
+                  dark: '#636061FF',
+                  basic: '#808080FF',
+                }
             },
             boxShadow: {
                 topNav: '0 -3px 0 0 #fd8383 inset',
