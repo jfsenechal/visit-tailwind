1a2b2ea1159dff98be9f52b0610f46809ef3e038
jfsenechal <jfsenechal@gmail.com>
2021-12-20 21:59:04 +0100

    partenaires

--- src/components/BannerParty.vue
+++ src/components/BannerParty.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="bg-party flex justify-start items-center relative text-white px-5 overflow-visible">
-    <img class="object-[initial] relative -top-8 h-48 w-80"
+    <img class="relative -top-8 h-48 w-80"
          src="https://visitmarche.be/wp-content/themes/visitmarche/assets/tartine/rsc/img/img_cat01.png" alt="party"/>
     <h2 class="text-2xl">Connaissez-vous nos différentes festivités ?</h2>
 
---
+++ src/components/Partenaires.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container grid grid-cols-[repeat(6,auto)] gap-2 justify-between items-center">
+    <a href="http://www.geoparkfamenneardenne.be/" class="">
+      <img src="https://visitmarche.be/wp-content/themes/visitmarche/assets/tartine/rsc/img/geopark.jpg" alt="geopark"
+           class="h-16 grayscale hover:grayscale-0"/>
+    </a>
+    <a href="https://www.famenneardenne.be/fr/" class="">
+      <img src="https://visitmarche.be/wp-content/themes/visitmarche/assets/tartine/rsc/img/img_sponsor01.png" alt="adl"
+           class="h-16 grayscale hover:grayscale-0"/>
+    </a>
+    <a href="https://cap.marche.be/" class="">
+      <img src="https://visitmarche.be/wp-content/themes/visitmarche/assets/tartine/rsc/img/img_sponsor02.png"
+           alt="famenne ardenne" class="h-16 grayscale hover:grayscale-0"/>
+    </a>
+    <a href="http://www.paysdefamenne.be/" class="">
+      <img src="https://visitmarche.be/wp-content/themes/visitmarche/assets/tartine/rsc/img/img_sponsor03.png"
+           alt="cap commerçants" class="h-16 grayscale hover:grayscale-0"/>
+    </a>
+    <a href="https://www.marche.be/economie" class="">
+      <img src="https://visitmarche.be/wp-content/themes/visitmarche/assets/tartine/rsc/img/img_sponsor04.png"
+           alt="mcfa" class="h-16 grayscale hover:grayscale-0"/>
+    </a>
+    <a href="http://mcfa.marche.be/" class="">
+      <img src="https://visitmarche.be/wp-content/themes/visitmarche/assets/tartine/rsc/img/img_sponsor05.png"
+           alt="pays famenne" class="h-16 grayscale hover:grayscale-0"/>
+    </a>
+  </div>
+
+</template>
