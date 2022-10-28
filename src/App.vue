afd20e348aebe8d3b2e5bf6a30ffbd4fea1b1a95
jfsenechal <jfsenechal@gmail.com>
2022-10-28 23:12:00 +0200

    draft

--- src/App.vue
+++ src/App.vue
@@ -11,23 +11,32 @@ import Footer from './components/Footer.vue'
 </script>
 
 <template>
-  <div class="bg-bg_home bg-cover bg-center">
-    <div class="fle4x w-full bghomecolor bg-opacity-60 mix-blend-sc4reen">
-      <Header/>
+  <Header/>
+  <main>
+    <div class="bg-home bg-no-repeat bg-cover bg-center h-[50rem]">
+      <div class="relative bghomecolor py-24">
+        <div class="flex flex-col justify-center items-center w-full pt-60">
+          <SearchHome/>
+          <IconesHome/>
+        </div>
+      </div>
+      <div class="flex flex-col justify-center items-center w-full">
+        <HomeCaractere/>
+        <section class="bg-white w-full mx-2 mx-auto grid grid-cols-1 xl:px-40 xl:grid-cols-[60%,40%] xl:gap-4">
+          <div>
+            <Inspiration/>
+          </div>
+          <div class="shadow-lg p-4">
+            <Agenda/>
+          </div>
+        </section>
+      </div>
     </div>
-    <main class="flex flex-col justify-center items-center w-full pt-60">
-      <SearchHome/>
-      <IconesHome/>
-      <HomeCaractere/>
-      <section class="bg-white w-full mx-2 mx-auto grid grid-cols-1 xl:px-40 xl:grid-cols-[60%,40%] xl:gap-4">
-        <div>
-          <Inspiration/>
-        </div>
-        <div class="shadow-lg p-4">
-          <Agenda/>
-        </div>
-      </section>
-    </main>
-  </div>
+  </main>
   <Footer/>
 </template>
+<style>
+.bghomecolor {
+  background: linear-gradient(90deg, rgba(77, 77, 77, 0.35), rgba(231, 218, 203, 0.35));
+}
+</style>
