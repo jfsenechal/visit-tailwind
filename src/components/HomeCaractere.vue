89537c2de4c1541c706aab28dcec64928ed4e8f9
jfsenechal <jfsenechal@gmail.com>
2022-10-28 22:53:03 +0200

    bg-pastel

--- src/components/HomeCaractere.vue
+++ src/components/HomeCaractere.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="bg-home relative mt-40 w-full text-center text-white flex flex-col justify-center items-center h-70">
+  <div class="relative bg-pastel mt-40 w-full text-center text-white flex flex-col justify-center items-center h-70">
     <span class="text-8xl relative -top-7">MARCHE</span>
     <span class="text-3xl">Ville de caractère</span>
-    <p class="py-6 text-caractere max-w-3xl text-normal">Au cœur de la nature se niche Marche-en-Famenne, une ville de caractère qui conjugue esprit artistique, charme et bonne humeur. On choisit Marche pour se détendre quelques jours ou partir à l’aventure. Voici quelques inspirations pour organiser votre séjour lors de votre prochaine pause.</p>
+    <p class="py-6 text-caractere max-w-3xl text-normal">Au coeur de la nature se niche Marche-en-Famenne, une ville de caractère qui conjugue esprit artistique, charme et bonne humeur. On choisit Marche pour se détendre quelques jours ou partir à l’aventure. Voici quelques inspirations pour organiser votre séjour lors de votre prochaine pause.</p>
   </div>
 </template>
