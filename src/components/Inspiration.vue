dbe9007a733d1487305acfaea94c933771a52d22
jfsenechal <jfsenechal@gmail.com>
2022-10-29 22:10:51 +0200

    real data

--- src/components/Inspiration.vue
+++ src/components/Inspiration.vue
@@ -1,3 +1,33 @@
+<script setup>
+import {onMounted, ref} from "vue";
+import {items} from "@/data/posts"
+
+const articles = ref([]);
+
+function getArticles() {
+  fetch("https://visitmarche.be/api/posts.php?id=2")
+      .then(function (response) {
+        // The API call was successful!
+        return response.json();
+      })
+      .then(function (data) {
+        // This is the JSON from our response
+        articles.value = data;
+      })
+      .catch(function (err) {
+        // There was an error
+        console.warn("Something went wrong.", err);
+      });
+}
+
+onMounted(() => {
+
+});
+
+function isEven(i) {
+  return i === 0 || i === 3
+}
+</script>
 <template>
   <h2
       class="py-2 px-4 text-center font-montserrat-bold text-2xl text-grey-dark lg:text-left xl:p-4"
@@ -5,52 +35,23 @@
     Inspiration
   </h2>
   <div class="grid grid-cols-3 grid-rows-2 gap-2">
-    <div class="col-span-2 hover:bg-cta-hover relative h-48 max-w-full overflow-hidden transition-all duration-500">
+    <div
+        v-for="(item,index) in items"
+        :key="item.ID"
+        class="hover:bg-cta-hover relative h-48 max-w-full overflow-hidden transition-all duration-500"
+        :class="{'col-span-2': isEven(index)}">
       <figure class="grid">
         <img
             class="mix-blend-lighten absolute inset-0 w-full h-full object-cover col-start-1 col-end-2 row-start-1 row-end-2 hover:scale-110 transition-all duration-500"
-            src="https://picsum.photos/800" alt=""/>
-        <figcaption class="col-start-1 col-end-2 row-start-1 row-end-2 self-end">This is a long caption flowing into
-          two lines or more.
-        </figcaption>
-      </figure>
-    </div>
-
-    <div class="hover:bg-cta-hover relative h-48 max-w-full overflow-hidden transition-all duration-500">
-      <figure class="grid">
-        <img
-            class="mix-blend-lighten absolute inset-0 w-full h-full object-cover col-start-1 col-end-2 row-start-1 row-end-2 hover:scale-110 transition-all duration-500"
-            src="https://picsum.photos/800" alt=""/>
-        <figcaption class="col-start-1 col-end-2 row-start-1 row-end-2 self-end">This is a long caption flowing into
-          two lines or more.
-        </figcaption>
-      </figure>
-    </div>
-
-    <div class="hover:bg-cta-hover relative h-48 max-w-full overflow-hidden transition-all duration-500">
-      <figure class="grid">
-        <img
-            class="mix-blend-lighten absolute inset-0 w-full h-full object-cover col-start-1 col-end-2 row-start-1 row-end-2 hover:scale-110 transition-all duration-500"
-            src="https://picsum.photos/800" alt=""/>
-        <figcaption class="col-start-1 col-end-2 row-start-1 row-end-2 self-end">This is a long caption flowing into
-          two lines or more.
-        </figcaption>
-      </figure>
-    </div>
-
-    <div class="col-span-2 hover:bg-cta-hover relative h-48 max-w-full overflow-hidden transition-all duration-500">
-      <figure class="grid">
-        <img
-            class="mix-blend-lighten absolute inset-0 w-full h-full object-cover col-start-1 col-end-2 row-start-1 row-end-2 hover:scale-110 transition-all duration-500"
-            src="https://picsum.photos/800" alt=""/>
-        <figcaption class="col-start-1 col-end-2 row-start-1 row-end-2 self-end">This is a long caption flowing into
-          two lines or more.
+            :src="item.thumbnail_url" alt=""/>
+        <figcaption class="col-start-1 col-end-2 row-start-1 row-end-2 self-end">
+          {{ item.post_title }} {{ index }} {{ index % 2 }}
         </figcaption>
       </figure>
     </div>
   </div>
   <a
-      class="ml-auto mr-2 block block w-56 items-center justify-center pt-4 font-montserrat-semi-bold text-cta-light transition-all duration-700 hover:mr-8"
+      class="ml-auto mr-2 block block w-72 items-center justify-center pt-4 font-montserrat-semi-bold text-cta-light transition-all duration-700 hover:mr-8"
       href="/"
       title="Plus d'actualités"
   >
@@ -66,5 +67,4 @@
 .x {
   background: linear-gradient(180deg, rgba(77, 77, 77, 0.35), rgba(231, 218, 203, 0.35));
 }
-
 </style>
