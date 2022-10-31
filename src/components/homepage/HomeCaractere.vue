20ccdcdfc2ef0588b072b9139725059cfdf6e9f0
jfsenechal <jfsenechal@gmail.com>
2022-10-31 13:53:31 +0100

    height

--- src/components/homepage/HomeCaractere.vue
+++ src/components/homepage/HomeCaractere.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="relative bg-pastel m4t-40 w-full text-center text-white flex flex-col justify-center items-center h-70">
+  <div class="relative bg-pastel w-full text-center text-white flex flex-col items-center h-72">
     <span class="text-8xl relative -top-[3.2rem] font-montserrat-semi-bold">MARCHE</span>
-    <span class="text-3xl">Ville de caractère</span>
+    <span class="text-3xl font-montserrat-semi-bold">Ville de caractère</span>
     <p class="py-6 text-caractere max-w-3xl text-normal">Au coeur de la nature se niche Marche-en-Famenne, une ville de caractère qui conjugue esprit artistique, charme et bonne humeur. On choisit Marche pour se détendre quelques jours ou partir à l’aventure. Voici quelques inspirations pour organiser votre séjour lors de votre prochaine pause.</p>
   </div>
 </template>
