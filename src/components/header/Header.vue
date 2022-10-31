1112994fc56f654c9c74a930356c8be6e7661455
jfsenechal <jfsenechal@gmail.com>
2022-10-31 13:35:48 +0100

    remake menu language

--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -1,41 +1,19 @@
 <script setup>
 import Navigation from '@/components/header/Navigation.vue'
-import IconesSocial from "@/components/header/IconesSocial.vue";
-</script>
+import IconesSocial from "@/components/header/IconesSocial.vue";</script>
 
 <template>
-  <header class="fixed top-0 right-0 left-0 flex justify-between items-center p-3 bghomecolor bg-opacity-60 z-20">
+  <header class="fixed top-0 right-0 left-0 flex justify-between items-center p-3 bg-home-color bg-opacity-60 z-20">
     <a href="#" class="flex flex-grow flex-shrink-0">
       <img class="w-24 inline" alt="logo"
            src="https://visitmarche.be/wp-content/themes/visitmarche/assets/tartine/rsc/img/img_logo.png"/>
     </a>
     <Navigation/>
-    <div class="flex px-7 font-montserrat-medium relative group">
-      <a href="#" class="inline-flex items-center text-white">FR
-        <svg class="text-white ml-2 h-5 w-5 group-hover:text-gray-500" xmlns="http://www.w3.org/2000/svg"
-             viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-          <path fill-rule="evenodd"
-                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                clip-rule="evenodd"/>
-        </svg>
-      </a>
-      <ul class="invisible bg-white text-caractere my-4 h-24 absolute top-7 flex flex-col justify-center items-center group-hover:visible transition-all duration-500 ease-in-out">
-        <li class="hover:text-cta-light before:triangle">
-          <a href="#" class="p-4">FR</a>
-        </li>
-        <li class="hover:text-cta-light">
-          <a href="#" class="p-4">NL</a>
-        </li>
-        <li class="hover:text-cta-light">
-          <a href="#" class="p-4">EN</a>
-        </li>
-      </ul>
-    </div>
     <IconesSocial/>
   </header>
 </template>
 <style>
-.bghomecolor {
+.bg-home-color {
   background: linear-gradient(90deg, rgba(77, 77, 77, 0.35), rgba(231, 218, 203, 0.35));
 }
 </style>
--- src/components/header/Navigation.vue
+++ src/components/header/Navigation.vue
@@ -35,6 +35,27 @@ https://css-tricks.com/in-praise-of-the-unambiguous-click-menu/
       <li class="border-transparent pb-2 nav-top">
         <a href="#">Pratique</a>
       </li>
+      <li class="border-transparent pb-2 nav-top group relative">
+        <a href="#" class="inline-flex items-center text-white">FR
+          <svg class="text-white ml-2 h-5 w-5 group-hover:text-gray-500" xmlns="http://www.w3.org/2000/svg"
+               viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+            <path fill-rule="evenodd"
+                  d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                  clip-rule="evenodd"/>
+          </svg>
+        </a>
+        <ul class="invisible rounded-md bg-white text-caractere my-4 h-24 absolute top-7 flex flex-col justify-center items-center group-hover:visible transition-all duration-500 ease-in-out">
+          <li class="hover:text-cta-light before:triangle">
+            <a href="#" class="p-4">FR</a>
+          </li>
+          <li class="hover:text-cta-light">
+            <a href="#" class="p-4">NL</a>
+          </li>
+          <li class="hover:text-cta-light">
+            <a href="#" class="p-4">EN</a>
+          </li>
+        </ul>
+      </li>
     </ul>
   </nav>
 </template>
@@ -44,18 +65,23 @@ li.nav-top {
   @apply transition-all duration-300 ease-in-out hover:border-b-4 hover:border-cta-light
 }
 
-/*
-<!-- triangle
-.tooltip:before {
+.triangle {
+  content: "";
   position: absolute;
-  right: 100%;
+  top: -6px;
+  background-color: #fff;
+  width: 15px;
+  height: 15px;
+  -webkit-clip-path: polygon(0 0, 100% 100%, 0 100%);
+  clip-path: polygon(0 0, 100% 100%, 0 100%);
+  transform: rotate(135deg);
 }
 
-.tooltip.to-right:before {
-  position: absolute;
-  right: auto;
-  left: 100%;
+.triangle-up {
+  width: 0;
+  height: 0;
+  border-left: 50px solid transparent;
+  border-right: 50px solid transparent;
+  border-bottom: 100px solid red;
 }
--->
-*/
 </style>
