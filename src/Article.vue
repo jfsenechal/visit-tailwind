ce5aeccf5135e0d0a3c6048cdfdc81695a264b66
jfsenechal <jfsenechal@gmail.com>
2022-10-31 21:25:50 +0100

    real data post

--- src/Article.vue
+++ src/Article.vue
@@ -5,24 +5,43 @@ import Body from "@/components/article/Body.vue";
 import Footer from "@/components/footer/Footer.vue";
 import Cadre from "@/components/article/Cadre.vue";
 import SeeAlso from "@/components/article/SeeAlso.vue";
+import {onMounted, ref} from "vue";
 
-const post = {
-  'post_title': 'Un panorama impressionnant!',
-  'excerpt': 'Tour Circus',
-  'category':'Inspirations',
-  'content': "Après d’importants travaux de rénovation, la figure emblématique du patrimoine marchois a été inaugurée et est désormais accessible au grand public.<br/> L’impressionnante Tour de la Famenne offre une vue imprenable sur la région de Marche et ses environs. Son accès est possible 7 jours/7 de 10h à 17h Pour 2€, vous pourrez contempler le panorama de la région"
+const post = ref([]);
+
+function getPost(id) {
+  fetch("https://visitmarche.be/api/post.php?id=" + id)
+      .then(function (response) {
+        // The API call was successful!
+        return response.json();
+      })
+      .then(function (data) {
+        // This is the JSON from our response
+        post.value = data;
+      })
+      .catch(function (err) {
+        // There was an error
+        console.warn("Something went wrong.", err);
+      });
 }
+
+onMounted(() => {
+  const id = new URL(window.location).searchParams.get("id")
+  getPost(id);
+});
+
 </script>
 <template>
   <Header/>
-  <HeaderArticle :title="post.category" :img="'https://visitmarche.be/wp-content/uploads/2021/02/Rencontres-scaled.jpg'"/>
+  <HeaderArticle :title="'Inspiration'"
+                 :img="post.image"/>
   <Cadre :excerpt="post.excerpt"/>
   <main class="container mx-auto">
     <article
         class="grid grid-cols-1 xl:grid-cols-2 items-start mt-2 xl:mt-4 mx-auto px-4">
       <div class="p-8">
         <h1 class="font-bold text-2xl text-caractere">{{ post.post_title }}</h1>
-        <Body/>
+        <Body :post="post"/>
       </div>
       <aside class="p-8">
         <SeeAlso/>
