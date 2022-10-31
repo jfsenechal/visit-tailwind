c347fdd8592f2befe05ad8527d973cec2b7589d6
jfsenechal <jfsenechal@gmail.com>
2022-10-31 09:04:41 +0100

    draft

--- src/components/article/ImageTop.vue
+++ src/components/article/ImageTop.vue
@@ -1,16 +1,36 @@
 <template>
   <figure class="grid h-[37rem] max-w-full">
     <div class="col-start-1 col-end-2 row-start-1 row-end-2 h-full w-full bg-gradient-img">
-      <img class="h-[37rem] col-start-1 col-end-2 row-start-1 row-end-2 mix-blend-normal" src="https://visit.marche.be/wp-content/uploads/2022/05/DJI_0075-1-scaled-1.jpg" alt=""/>
+      <img class="h-[37rem] col-start-1 col-end-2 row-start-1 row-end-2 mix-blend-normal"
+           src="https://visit.marche.be/wp-content/uploads/2022/05/DJI_0075-1-scaled-1.jpg" alt=""/>
     </div>
-    <figcaption class="col-start-1 col-end-2 row-start-1 row-end-2 self-end flex flex-col justify-end items-start px-16 pb-6 ">
-      <a href="https://visit.marche.be/categorie/inspirations/" class="text-cta-light text-lg self-start font-extrabold py-4">
+    <figcaption
+        class="col-start-1 col-end-2 row-start-1 row-end-2 self-end flex flex-col justify-end items-start px-16 pb-6 ">
+      <a href="https://visit.marche.be/categorie/inspirations/"
+         class="text-cta-light text-lg self-start font-extrabold py-4">
         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 inline" viewbox="0 0 20 20" fill="currentColor">
-          <path fill-rule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd"/>
+          <path fill-rule="evenodd"
+                d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
+                clip-rule="evenodd"/>
         </svg>
-        Retour à Inspirations
+        Retour à Inspirations3
       </a>
-      <h2 class="text-white text-3xl leading-5 font-semibold">Inspirations</h2>
+      <h2 class="text-white text-3xl leading-5 font-semibold">Inspirations3</h2>
     </figcaption>
   </figure>
 </template>
+<style>
+
+.bg-gradient-img {
+  background: linear-gradient(
+      90deg,
+      rgba(77, 77, 77, 0.35),
+      rgba(231, 218, 203, 0.35)
+  );
+}
+
+figure {
+  @apply m-4 max-h-[18rem];
+}
+
+</style>
