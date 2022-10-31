6c7134a072a73f9e4082dcbcbf3992edc619e6ab
jfsenechal <jfsenechal@gmail.com>
2022-10-31 14:28:13 +0100

    draft menu mobile

--- src/Draft.vue
+++ src/Draft.vue
@@ -1,8 +1,67 @@
 <script setup>
 
+import NavIconHomeMobile from '@/components/header/mobile/NavIconHomeMobile.vue'
+import NavIconMenuMobileSvg from '@/components/header/mobile/NavIconMenuMobileSvg.vue'
+import NavIconCommuneShortCutMobile from "@/components/header/mobile/NavIconCommuneShortCutMobile.vue";
+import NavItemsMobile from "@/components/header/mobile/NavItemsMobile.vue";
+import NavIconSearchMobile from "@/components/header/mobile/NavIconSearchMobile.vue";
 
+function openMenuVivre() {
+  const menu = document.querySelector("#menu-organise");
+  menu.style.top = "0";
+}
 </script>
 
 <template>
-
+<nav class="musshadow fixed bottom-0 top-auto right-0 left-0 xl:static xl:shadow-none">
+    <ul class="grid grid-cols-5 items-center justify-evenly bg-cta-dark text-white xl:flex xl:bg-white xl:text-cta-dark"
+        id="navigationTop">
+      <NavIconHomeMobile />
+      <NavIconSearchMobile />
+       <li
+        class="nav-top xl:font-montserrat-semi-bold xl:transition-all xl:duration-500 btn-vivre">
+        <a href="#" class="hidden xl:flex">
+          <NavIconMenuMobileSvg />
+        </a>
+        <label for="checkboxMenu" class="xl:hidden icon-menu-mobile cursor-pointer">
+          <button name="menu" @click="openMenuVivre">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                 stroke="currentColor" class="w-6 h-6">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+            </svg>
+          </button>
+          <span class="text-menu-mobile">Organise</span>
+        </label>
+        <NavItemsMobile />
+      </li>
+      <NavIconCommuneShortCutMobile />
+    </ul>
+</nav>
 </template>
+<style>
+
+li.nav-top {
+  @apply xl:p-3 xl:text-cta-dark
+  xl:transition-shadow xl:duration-300 xl:ease-in-out;
+}
+
+li.nav-top:hover {
+  box-shadow: 0 -3px 0 0 #4ae0c7 inset;
+}
+
+.btn-vivre:hover > nav {
+  @apply xl:block
+}
+
+.icon-menu-mobile {
+  @apply my-4 flex flex-col flex-wrap items-center justify-center font-montserrat-semi-bold;
+}
+
+.text-menu-mobile {
+  @apply text-xs font-montserrat-light;
+}
+
+.musshadow {
+  box-shadow: 0 -0.125rem 0.45rem rgba(0, 0, 0, 0.75);
+}
+</style>
---
+++ src/components/header/mobile/NavItemsMobile.vue
@@ -0,0 +1,21 @@
+<template>
+  <nav
+      class="xl:hidden fixed bg-cta-dark top-full bottom-0 left-0 right-0 h-full xl:h-auto xl:top-16 xl:bottom-auto xl:pb-4 w-full transition-all duration-500"
+      id="menu-organise">
+    <ul
+        class="flex flex-col items-start xl:items-stretch justify-center h-full xl:h-auto mt-3 box-border xl:max-w-[50%]">
+      <li class="before:triangle before:left-10 hover:text-cta-light">
+        <a href="#" class="p-2">Découvrir</a>
+      </li>
+      <li class="hover:text-cta-light">
+        <a href="#" class="p-2">Savourer</a>
+      </li>
+      <li class="hover:text-cta-light">
+        <a href="#" class="p-2">Bouger</a>
+      </li>
+      <li class="hover:text-cta-light">
+        <a href="#" class="p-2">Séjourner</a>
+      </li>
+    </ul>
+  </nav>
+</template>
