5bd039c0bd9a9f5b1826e4ec20459bdbec1be244
jfsenechal <jfsenechal@gmail.com>
2022-11-02 20:07:27 +0100

    popup search xl

--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -1,19 +1,41 @@
 <script setup>
+import { ref } from "vue";
 import Navigation from '@/components/header/Navigation.vue'
 import IconesSocial from "@/components/header/IconesSocial.vue";
 import LanguagesSelect from "@/components/header/LanguagesSelect.vue";
+import PopupSearch from "@/components/header/PopupSearch.vue";
+
+const searchIsOpen = ref(false);
+
+function openSearch() {
+  console.log("open");
+  console.log(searchIsOpen.value);
+  if (searchIsOpen.value === false) {
+    searchIsOpen.value = true;
+  }
+}
+
+function closeSearch() {
+  console.log("close");
+  if (searchIsOpen.value === true) {
+    console.log("close2");
+    searchIsOpen.value = false;
+  }
+}
 </script>
 
 <template>
   <header class="fixed top-0 right-0 left-0 flex justify-end items-center p-3 bg-home-color bg-opacity-60 z-20">
     <a href="#" class="flex flex-grow flex-shrink-0 xl:mr-auto">
       <img class="h-10 w-16 xl:h-14 xl:w-24 inline" alt="logo"
-           src="https://visitmarche.be/wp-content/themes/visitmarche/assets/tartine/rsc/img/img_logo.png"/>
+           src="images/visit_logo.png"/>
     </a>
-    <Navigation/>
+    <Navigation @open-search="openSearch"/>
     <LanguagesSelect/>
     <IconesSocial/>
   </header>
+  <PopupSearch @close-search=closeSearch @open-search="openSearch" :searchIsOpen="searchIsOpen" />
+
 </template>
 <style>
 .bg-home-color {
--- src/components/header/NavItemsXl.vue
+++ src/components/header/NavItemsXl.vue
@@ -38,6 +38,13 @@ defineEmits(["open-search"]);
   <li class="hidden xl:list-item border-transparent pb-2 nav-top-xl">
     <a href="#">Pratique</a>
   </li>
+  <li class="hidden xl:list-item border-transparent pb-2 nav-top-xl">
+    <a href="#" @click.prevent="$emit('open-search')">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+      </svg>
+    </a>
+  </li>
 </template>
 <style>
 li.nav-top-xl {
