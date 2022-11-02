8125e555eb52f99d7738b3cf11f35f4cc09a9d31
jfsenechal <jfsenechal@gmail.com>
2022-11-02 14:07:12 +0100

    smaller

--- src/components/article/Cadre.vue
+++ src/components/article/Cadre.vue
@@ -3,8 +3,8 @@ const props = defineProps(['excerpt'])
 </script>
 <template>
   <div class="bg-party w-full">
-    <div class="relative flex pt-6 px-6 pb-32 lg:p-12 flex-wrap z-10 justify-end lg:justify-start mw-[1640px] mx-auto">
-      <p class="text-2xl text-white px-0" style="font-weight: 600;letter-spacing: 0.1rem; color:#808080FF;">
+    <div class="relative flex pt-6 px-6 py-12 xl:pb-20 lg:p-12 flex-wrap z-10 justify-end lg:justify-start mx-auto">
+      <p class="text-lg xl:text-2xl text-white px-0" style="font-weight: 600;letter-spacing: 0.1rem; color:#808080FF;">
         {{ props.excerpt }}
       </p>
     </div>
