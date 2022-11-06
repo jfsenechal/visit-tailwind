1ad6f3cc4a4c7a3c3d6a1e0342cad7567740b98b
jfsenechal <jfsenechal@gmail.com>
2022-11-06 21:14:13 +0100

    clean bg

--- tailwind.config.js
+++ tailwind.config.js
@@ -62,14 +62,6 @@ module.exports = {
             flex: {
                 full: '100% 1 1',
             },
-            backgroundImage: {
-                'home': "url('https://visitmarche.be/wp-content/themes/visitmarche/assets/tartine/rsc/img/bg_home.jpg')",
-                'chasse': "url('https://visitmarche.be/wp-content/uploads/2021/09/DSC_0504-scaled.jpg')",
-                'escape': "url('https://visitmarche.be/wp-content/uploads/2021/10/4index.png')",
-                'noel': "url('https://visitmarche.be/wp-content/uploads/2021/11/christmas-g22147f560_1280.png')",
-                'totemus': "url('https://visitmarche.be/wp-content/uploads/2021/05/Chasse-au-tre%E2%95%A0usor.jpeg')",
-                'inspiration': 'linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url("https://images.pexels.com/photos/952479/pexels-photo-952479.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260)'
-            },
             objectPosition: {
                 'top-center': 'top center',
             }
