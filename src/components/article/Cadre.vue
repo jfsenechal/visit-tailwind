da694792487ca892bd7a0de4d91871d3cd38b521
jfsenechal <jfsenechal@gmail.com>
2022-11-08 20:41:38 +0100

    xl mobile ok

--- src/components/article/Cadre.vue
+++ src/components/article/Cadre.vue
@@ -4,12 +4,12 @@ const excerpt2 = 'Marche-en-Famenne aime l’art, ainsi on y découvre des sculp
 </script>
 <template>
   <div class="bg-party w-full">
-    <div class="relative flex flex-wrap z-10 h-36 items-center justify-end lg:justify-center mx-auto">
+    <div class="relative flex flex-col p-4 xl:flex-row flex-wrap z-10 xl:h-36 items-center xl:justify-center mx-auto">
       <span class="text-lg xl:text-xl text-caractere font-montserrat-semi-bold" v-html="props.excerpt ?? excerpt2">
 
       </span>
       <img src="images/statue.png" alt="image"
-           class="absolute xl:top-0 xl:bottom-0 right-12 h-52 ">
+           class="h-40 xl:absolute xl:top-0 xl:bottom-0 xl:right-12 xl:h-52 ">
     </div>
   </div>
 </template>
